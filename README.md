ac5b2f587f3144f1eabd7d56ed0b75b7d8db53f4
Steveyboi <thenewgwe@gmail.com>
2023-12-04 07:58:11 +0700

    Add bit data labels

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ Label files use the syntax created by [user202729](https://github.com/user202729
 01234		function_label
 f_02345		function_label
 .l_002		.local_label
-d_45678		data_label		# comment
+d_45678		data_label
+data_label.1		bit_data_label
 # comment
 ```
 
@@ -20,4 +21,4 @@ Loads labels from a file-like object.
 Arguments:
 - `f`: A file-like object to load label data from. Should be in text read mode.
 
-Returns: 2 dictionaries for function + local labels and data labels respectively.
+Returns: 3 dictionaries for function + local labels, data labels and bit data labels respectively.
--- labeltool.py
+++ labeltool.py
@@ -2,7 +2,7 @@ import re
 import sys
 import logging
 
-def load_labels(f: open):
+def load_labels(f: open, start: int):
 	label_data = []
 	labels = f.readlines()
 	f.close()
@@ -11,34 +11,39 @@ def load_labels(f: open):
 
 	labels = {}
 	data_labels = {}
+	data_bit_labels = {}
 
 	curr_func = None
 	for data in label_data:
 		if len(data) > 1:
 			try:
 				if data[0].startswith('f_'):
-					addr = int(data[0][2:], 16)
+					addr = int(data[0][2:], 16) - start
 					if addr in labels: logging.warning(f'Duplicate function label {addr:05X}, skipping')
 					else:
 						labels[addr] = [data[1], True]
 						curr_func = addr
 				elif data[0].startswith('.l_'):
-					addr = curr_func + int(data[0][3:], 16)
+					addr = curr_func + int(data[0][3:], 16) - start
 					if addr in labels: logging.warning(f'Duplicate local label {curr_func:05X}+{int(data[0][3:], 16):03X}, skipping')
 					else: labels[addr] = [data[1], False, 0, []]
 				elif data[0].startswith('d_'):
-					addr = int(data[0][2:], 16)
+					addr = int(data[0][2:], 16) - start
 					if addr in data_labels: logging.warning(f'Duplicate data label {addr:05X}, skipping')
 					else: data_labels[addr] = data[1]
 				else:
-					addr = int(data[0], 16)
-					if addr in labels: logging.warning(f'Duplicate function label {addr:05X}, skipping')
-					else:
-						labels[addr] = [data[1], True]
-						curr_func = addr
+					try:
+						addr = int(data[0], 16) - start
+						if addr in labels: logging.warning(f'Duplicate function label {addr:05X}, skipping')
+						else:
+							labels[addr] = [data[1], True]
+							curr_func = addr
+					except ValueError:
+						if data[0] in data_bit_labels: logging.warning(f'Duplicate bit data label {data[0]}, skipping')
+						else: data_bit_labels[data[0]] = data[1]
 			except Exception as e: logging.warning(f'Exception occured: {str(e)} [{type(e).__name__}]')
 
-	return labels, data_labels
+	return labels, data_labels, data_bit_labels
 
 if __name__ == '__main__':
 	print('Command line interface coming soon!')
